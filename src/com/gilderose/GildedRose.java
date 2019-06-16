998ea6231a2d9ae786f4926bf0deae213d10785b
vargasped <p.varper@gmail.com>
2019-06-15 21:41:06 -0400

    renombrado el metodo validateItemName a validateEnumItem

--- src/com/gilderose/GildedRose.java
+++ src/com/gilderose/GildedRose.java
@@ -10,10 +10,10 @@ public class GildedRose {
 
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
-			if (!validateItemName(items[i],EnumItem.AGED_BRIE)
-					&& !validateItemName(items[i],EnumItem.BACKSTAGE_PASSESS_TAFKAL80ETC_CONCERT)) {
+			if (!validateEnumItem(items[i],EnumItem.AGED_BRIE)
+					&& !validateEnumItem(items[i],EnumItem.BACKSTAGE_PASSESS_TAFKAL80ETC_CONCERT)) {
 				if (items[i].quality > 0) {
-					if (!validateItemName(items[i],EnumItem.SULFURAS_HAND_OF_RAGNAROS)) {
+					if (!validateEnumItem(items[i],EnumItem.SULFURAS_HAND_OF_RAGNAROS)) {
 						items[i].quality = items[i].quality - 1;
 					}
 				}
@@ -21,7 +21,7 @@ public class GildedRose {
 				if (items[i].quality < 50) {
 					items[i].quality = items[i].quality + 1;
 
-					if (validateItemName(items[i],EnumItem.BACKSTAGE_PASSESS_TAFKAL80ETC_CONCERT)) {
+					if (validateEnumItem(items[i],EnumItem.BACKSTAGE_PASSESS_TAFKAL80ETC_CONCERT)) {
 						if (items[i].sellIn < 11) {
 							if (items[i].quality < 50) {
 								items[i].quality = items[i].quality + 1;
@@ -37,15 +37,15 @@ public class GildedRose {
 				}
 			}
 
-			if (!validateItemName(items[i],EnumItem.SULFURAS_HAND_OF_RAGNAROS)) {
+			if (!validateEnumItem(items[i],EnumItem.SULFURAS_HAND_OF_RAGNAROS)) {
 				items[i].sellIn = items[i].sellIn - 1;
 			}
 
 			if (items[i].sellIn < 0) {
-				if (!validateItemName(items[i],EnumItem.AGED_BRIE)) {
-					if (!validateItemName(items[i],EnumItem.BACKSTAGE_PASSESS_TAFKAL80ETC_CONCERT)) {
+				if (!validateEnumItem(items[i],EnumItem.AGED_BRIE)) {
+					if (!validateEnumItem(items[i],EnumItem.BACKSTAGE_PASSESS_TAFKAL80ETC_CONCERT)) {
 						if (items[i].quality > 0) {
-							if (!validateItemName(items[i],EnumItem.SULFURAS_HAND_OF_RAGNAROS)) {
+							if (!validateEnumItem(items[i],EnumItem.SULFURAS_HAND_OF_RAGNAROS)) {
 								items[i].quality = items[i].quality - 1;
 							}
 						}
@@ -61,7 +61,7 @@ public class GildedRose {
 		}
 	}
 	
-	public boolean validateItemName(Item item, EnumItem enumItem) {
+	public boolean validateEnumItem(Item item, EnumItem enumItem) {
 		boolean validate= false;
 		
 		if(item.getEnumItem()==enumItem) 
